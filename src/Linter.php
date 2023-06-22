7bcace4139cc147434d669be7820b6b663ef3746
Volkan Welp <volkanwelp@gmail.com>
2023-06-22 23:01:20 +0200

    ✨ Add basis

--- src/Linter.php
+++ src/Linter.php
@@ -6,6 +6,26 @@ class Linter
 {
     public final function run($argv)
     {
-        echo "Check started.";
+        $this->checkFiles($argv);
+    }
+
+    private function checkFiles($argv)
+    {
+        $files = $this->getFiles();
+        foreach ($files as $file) {
+            $this->checkFile($file);
+        }
+    }
+
+    private function getFiles()
+    {
+        return glob(__DIR__ . '/../../../*.tpl');
+    }
+
+    private function checkFile($file)
+    {
+        echo $file . PHP_EOL;
+        // $contents = file_get_contents($file);
+        // $this->checkContents($contents);
     }
 }
